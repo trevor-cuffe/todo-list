18443e772e4a3d726dc11335c3031c930f7439dc
trevor-cuffe <trevor.d.cuffe@gmail.com>
2020-12-05 12:26:39 -0500

    add methods to create new todo elements

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,16 +1,19 @@
 // document.addEventListener("DOMContentLoaded", function() {
     //*** variable declarations ***//
     const themeToggle = document.querySelector("#light-dark-toggle");
+    const allTodos = document.querySelector("#all-todos");
     const todosLists = document.querySelectorAll(".todos-list");
-    console.log(todosLists);
     const checkboxes = document.querySelectorAll(".todo-checkbox");
+    const newTodoInput = document.querySelector(".todo-new form");
 
     //*** event listeners ***//
+    //Toggle Theme
     themeToggle.addEventListener('click', toggleTheme);
+
+    //Checkbox on/off
     for (list of todosLists) {
         list.addEventListener('click', function(event) {
             let targetElement = event.target;
-            console.log(targetElement);
             let selector = 'todo-checkbox';
             if(targetElement.classList.contains(selector)) {
                 const todo = targetElement.parentElement;
@@ -19,7 +22,30 @@
         });
     }
 
-    
+    //New Todo Submit
+    newTodoInput.addEventListener('submit', function(e) {
+        console.log("submit");
+        //prevent default event response
+        e.preventDefault();
+        const todoElement = this.parentNode.parentNode;
+        console.log(todoElement);
+        const todoTitleElement = this.querySelector(".todo-input");
+        const completed = todoElement.classList.contains("todo-complete");
+        const title = todoTitleElement.value;
+
+        //create new todo
+        const newTodo = createTodo(title, completed);
+
+        //append new todo
+        allTodos.appendChild(newTodo);
+
+        //clear input form
+        todoTitleElement.value = "";
+        todoElement.classList.remove("todo-complete");
+
+    });
+
+
     // RUN ON LOAD
     init();
 
@@ -63,6 +89,37 @@
             themeToggle.setAttribute('src', 'images/icon-sun.svg');
         }
 
+    }
+
+    function createTodo(title, completed) {
+        
+        //create todo element
+        let newTodo = document.createElement('li');
+        newTodo.classList.add("todo");
+        //set completion status
+        if(completed) newTodo.classList.add("todo-complete");
+
+        //create checkbox element
+        let checkbox = document.createElement('button');
+        console.dir(checkbox);
+        checkbox.classList.add("todo-checkbox");
+        checkbox.role = "checkbox";
+        //add checkbox to todo
+        newTodo.appendChild(checkbox);
+
+        //create content element
+        let todoContent = document.createElement('div');
+        todoContent.classList.add("todo-content");
+        //create title element
+        let todoTitle = document.createElement('span');
+        todoTitle.classList.add('todo-title');
+        todoTitle.innerText = title;
+        //add title to content
+        todoContent.appendChild(todoTitle);
+        //add content to todo
+        newTodo.appendChild(todoContent);
+
+        return newTodo;
     }
 
 
